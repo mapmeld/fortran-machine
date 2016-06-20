e0fb50f718b867351d8726d53163e445e9b1c942
Nick Doiron <ndoiron@mapmeld.com>
2016-06-19 20:29:56 -0400

    return all marsupials

--- marsupial.f90
+++ marsupial.f90
@@ -52,6 +52,33 @@ module marsupial
       call sqlite3_get_column(column(2), latinName)
       call sqlite3_get_column(column(3), wikiLink)
       call sqlite3_get_column(column(4), description)
+      exit
+    end do
+  endsubroutine
+
+  subroutine getAllMarsupials(name, latinName, wikiLink, description)
+    ! columns
+    character(len=50), dimension(8)	:: name, latinName, wikiLink, description
+
+    call sqlite3_open('marsupials.sqlite3', db)
+
+    allocate( column(4) )
+    call sqlite3_column_query( column(1), 'name', SQLITE_CHAR )
+    call sqlite3_column_query( column(2), 'latinName', SQLITE_CHAR )
+    call sqlite3_column_query( column(3), 'wikiLink', SQLITE_CHAR )
+    call sqlite3_column_query( column(4), 'description', SQLITE_CHAR )
+
+    call sqlite3_prepare_select( db, 'marsupials', column, stmt, "WHERE 1=1 LIMIT 8")
+
+    i = 1
+    do
+      call sqlite3_next_row(stmt, column, finished)
+      if (finished) exit
+
+      call sqlite3_get_column(column(1), name(i))
+      call sqlite3_get_column(column(2), latinName(i))
+      call sqlite3_get_column(column(3), wikiLink(i))
+      call sqlite3_get_column(column(4), description(i))
       i = i + 1
     end do
   endsubroutine
