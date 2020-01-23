82bfb051383cc27e1ce6030fe07e921f2fbdd025
Rudolf Polzer <divVerent@gmail.com>
2020-01-23 10:39:47 -0800

    cgi_decode_string: do not crash if the string contains a malformed escape sequence.
    Also, decode strings that contain percent signs in the output (like %2525)
    correctly, i.e. unescape only once.

--- flibs-0.9/flibs/src/cgi/cgi_protocol.f90
+++ flibs-0.9/flibs/src/cgi/cgi_protocol.f90
@@ -315,6 +315,9 @@ subroutine cgi_decode_string( string )
 
     integer :: k
     integer :: ch
+    integer :: n
+    integer :: p
+    integer :: ierr
 
     !
     ! First the +'s
@@ -329,12 +332,20 @@ subroutine cgi_decode_string( string )
     !
     ! Now %xx
     !
+    n = len(string)
+    p = 1
     do
-        k = index( string, '%' )
-        if ( k .le. 0 ) exit
+        k = index( string(p:), '%' ) + p - 1
+        if ( k .lt. p .or. k+2 .gt. n) exit
 
-        read( string(k+1:k+2), '(z2)' ) ch
+        read( string(k+1:k+2), '(z2)', iostat=ierr ) ch
+        if ( ierr /= 0 ) then
+            p = k + 1
+            cycle
+        endif
         string(k:) = achar(ch) // string(k+3:)
+        n = n - 2
+        p = k + 1
     enddo
 end subroutine cgi_decode_string
 
